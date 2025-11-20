11c46b559477dc98e29f88107bea87906603dc4c
Yosep Bima Aprillian <byosep050@gmail.com>
2025-11-20 08:48:56 +0700

    Fix Nav-bar

--- header.php
+++ header.php
@@ -9,21 +9,17 @@ $current = basename($_SERVER['PHP_SELF']);
 					<img src="images/logo.png" alt="Politeknik Negeri Malang" />
 				</a>
 			</div>
-		<div class="search">
-			<form>
-				<input type="submit" value="">
-				<input type="text" value="" placeholder="Search...">
-			</form>
+
 		</div>
 		<div class="top-menu">
 			<span class="menu"><img src="images/menu.png" alt=""></span>
 			<ul class="nav1">
-				<li class="<?php echo $current=='index.php' ? 'active' : ''; ?>"><a href="index.php">Home</a></li>
-				<li class="<?php echo $current=='about.php' ? 'active' : ''; ?>"><a href="about.php">About</a></li>
+				<li class="<?php echo $current=='index.php' ? 'active' : ''; ?>"><a href="index.php">Beranda</a></li>
+				<li class="<?php echo $current=='about.php' ? 'active' : ''; ?>"><a href="about.php">Profil Lab</a></li>
 				<li><a class="scroll" href="#services">Services</a></li>
-				<li class="<?php echo $current=='typo.php' ? 'active' : ''; ?>"><a href="typo.php">News</a></li>
-				<li class="<?php echo $current=='gallery.php' ? 'active' : ''; ?>"><a href="gallery.php">Gallery</a></li>
-				<li class="<?php echo $current=='contact.php' ? 'active' : ''; ?>"><a href="contact.php">Mail</a></li>
+				<li class="<?php echo $current=='typo.php' ? 'active' : ''; ?>"><a href="typo.php">Anggota & Riset</a></li>
+				<li class="<?php echo $current=='gallery.php' ? 'active' : ''; ?>"><a href="gallery.php">Berita & Aktivitas</a></li>
+				<li class="<?php echo $current=='contact.php' ? 'active' : ''; ?>"><a href="contact.php">Join Us!</a></li>
 			</ul>
 		</div>
 		<script>
