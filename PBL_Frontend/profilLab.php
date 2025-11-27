7ca94594f547b62fcd9bd22e20f71477db224cd3
Yosep Bima Aprillian <byosep050@gmail.com>
2025-11-27 19:39:47 +0700

    nambah footer

--- PBL_Frontend/profilLab.php
+++ PBL_Frontend/profilLab.php
@@ -145,6 +145,8 @@
         </div>
     </div>
 
+    <?php include __DIR__ . '/footer.php'; ?>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
