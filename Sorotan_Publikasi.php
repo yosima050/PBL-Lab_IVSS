5079d817bc9e1f08ba84029d59a81597a261078b
Ubaidillah <Ubaidillahulil32@gmail.com>
2025-11-27 13:53:21 +0700

    Sorotan_Publikasi

---
+++ Sorotan_Publikasi.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sorotan Publikasi - Lab IVSS</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href="dashboard/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/styleSP.css">
+    <link rel="stylesheet" href="PBL_Frontend/navbar.css">
+</head>
+
+<body>
+    <?php include 'PBL_Frontend/navbar.php'; ?>
+<div class="banner1"> </div>
+    <div class="content">
+        <div class="container">
+            <div class="tag1">
+                <span>Fokus Riset</span>
+                <img src="Asset/logo.png" class="emoji">
+            </div>
+
+            <div>
+                <span class="tag-badge">Intelligent Vision</span>
+                <span class="tag-badge">Smart Systems</span>
+            </div>
+
+            <div class="tag2">
+                <span>Sorotan Publikasi</span>
+                <img src="Asset/logo.png" class="emoji1">
+            </div>
+        </div>
+    </div>
+
+    <div class="container1">
+        <div class="bangzek"> 
+            <a href="#" target="_blank" class="btn btn-outline-primary btn-sm">Latest</a>
+            <a href="#" target="_blank" class="btn btn-outline-primary btn-sm">Oldest</a>
+            <a href="#" target="_blank" class="btn btn-outline-primary btn-sm">Years</a>
+        </div>
+    </div>
+
+    <div class="row justify-content-center-bos">
+                <div class="pub-cards-container">
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Segmentasi berbasis k-means pada deteksi citra penyakit daun tanaman jagung</h3>
+                        <p class="pub-date">15 Nov 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Klasifikasi Jenis Kelamin Pada Citra Wajah Menggunakan Metode Naive Bayes</h3>
+                        <p class="pub-date">25 Nov 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Sistem Pengambil Keputusan Rekomendasi Lokasi Wisata Malang Raya Dengan Metode MOORA</h3>
+                        <p class="pub-date">3 Dec 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Penerapan Facial Landmark Point Untuk Klasifikasi Jenis Kelamin Berdasarkan Citra Wajah</h3>
+                        <p class="pub-date">15 Dec 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Comparison of recognition accuracy on dynamic hand gesture using feature selection</h3>
+                        <p class="pub-date">10 Dec 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                    <div class="pub-card-item">
+                        <h3 class="pub-title">Subpixel subtle motion estimation of micro-expressions multiclass classification</h3>
+                        <p class="pub-date">28 Dec 2025</p>
+                        <a href="#" target="_blank" class="pub-button">Baca</a>
+                    </div>
+                </div>
+        </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
